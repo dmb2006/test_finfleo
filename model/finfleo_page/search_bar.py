fb3f1bcb75208f53e495b60db11c3c249fcdb7f5
Aleksandr Minin <dmb2006minin@yandex.ru>
2025-07-23 10:43:47 +0300

    Добавление screenshot и step для search_in_bar

--- model/finfleo_page/search_bar.py
+++ model/finfleo_page/search_bar.py
@@ -1,8 +1,12 @@
+import allure
 from selene import browser, have
 
 
 class Search_bar:
     def search_in_bar(self):
-        browser.open('https://finleo.ru')
-        browser.all('input[type=text]')[0].send_keys('БелАЗ').press_enter()
-        browser.element('[class="search-bar__results--empty"]').should(have.exact_text('Ничего не найдено'))
+        with allure.step('Открытие страницы'):
+            browser.open('https://finleo.ru')
+        with allure.step(f'Ввод в поисковую строку {'БелАЗ'}'):
+            browser.all('input[type=text]')[0].send_keys('БелАЗ').press_enter()
+        with allure.step('Проверка, что по данному запросу ничего не найдено'):
+            browser.element('[class="search-bar__results--empty"]').should(have.exact_text('Ничего не найдено'))
